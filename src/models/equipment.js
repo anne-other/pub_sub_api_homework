fe257d41351a3aa43a2332f181e95e4e55ad8269
anne-other <anne.c.ince@gmail.com>
2018-09-01 12:42:57 +0100

    adding the basics to the equipment model

---
+++ src/models/equipment.js
@@ -0,0 +1,8 @@
+const PubSub = require('../helpers/pub_sub.js');
+const Request = require('../helpers/request.js');
+
+const Equipment = function () {
+  this.equipment = [];
+}
+
+module.exports = Equipment;
