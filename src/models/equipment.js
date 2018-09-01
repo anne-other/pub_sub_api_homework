072212ee5b372d80f90814c2011f980fbd5c1625
anne-other <anne.c.ince@gmail.com>
2018-09-01 12:54:57 +0100

    publishing the data from the model and putting in a catch for an error

--- src/models/equipment.js
+++ src/models/equipment.js
@@ -9,9 +9,21 @@ Equipment.prototype.getEquipment = function () {
   const request = new Request("http://www.dnd5eapi.co/api/equipment")
   request.get().then((data) => {
     this.data = data;
-    console.log(this.data);
+    PubSub.publish('Equipment:equipment-data-loaded', this.data);
+  }).catch((error) => {
+    console.error(error);
   })
 };
 
+Dogs.prototype.getData = function (breed) {
+  const url = `https://dog.ceo/api/breed/${ breed }/images`;
+  const request = new Request(url);
+  request.get().then((data) => {
+    this.data = data.message;
+    PubSub.publish('Dogs:dog-data-loaded', this.data);
+  }).catch((err) => {
+    console.error(err);
+  });
+}
 
 module.exports = Equipment;
